7c245d8154d7da4111e3e2126759617620db6210
FenderMora <95876008+FenderMora@users.noreply.github.com>
2024-05-25 13:59:41 -0600

    first commit

---
+++ Nobel_Console_Application/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nobel_Console_Application.Services;
+
+namespace Nobel_Console_Application
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                TopicsServices topics = new TopicsServices();
+                var url = "https://api.sampleapis.com/codingresources/codingResources";
+                var uniqueTopics = await topics.GetUniqueTopicsAsync(url);
+                topics.PrintUniqueTopics(uniqueTopics);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Nobel_Console_Application/Services/TopicsServices.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+
+namespace Nobel_Console_Application.Services
+{
+	public class TopicsServices
+	{
+
+        public async Task<HashSet<string>> GetUniqueTopicsAsync(string url)
+        {
+            using HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(url);
+            return ExtractUniqueTopics(response);
+        }
+
+        public HashSet<string> ExtractUniqueTopics(string jsonResponse)
+        {
+            var uniqueTopics = new HashSet<string>();
+
+            var jsonDocument = JsonDocument.Parse(jsonResponse);
+            if (jsonDocument.RootElement.ValueKind == JsonValueKind.Array)
+            {
+                foreach (var element in jsonDocument.RootElement.EnumerateArray())
+                {
+                    var topics = element.GetProperty("topics").EnumerateArray();
+                    foreach (var topic in topics)
+                    {
+                        uniqueTopics.Add(topic.GetString());
+                    }
+                }
+            }
+            return uniqueTopics;
+        }
+
+        public void PrintUniqueTopics(HashSet<string> uniqueTopics)
+        {
+            Console.WriteLine("Unique Topics:");
+            foreach (var topic in uniqueTopics)
+            {
+                Console.WriteLine(topic);
+            }
+        }
+    }
+}
+
